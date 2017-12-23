e90751101b4fb13cd854801b47fee1daa7172f11
Marc Wood <metamarcdw@gmail.com>
2017-12-22 21:17:26 -0600

    Add code to shutdown gracefully on Ctrl-C.

--- nowallet/__main__.py
+++ nowallet/__main__.py
@@ -74,11 +74,43 @@ def main():
             decimal_fee, chain.chain_1209k.upper()))
         print("Transaction sent!\nID: {}".format(txid))
 
-    asyncio.ensure_future(wallet.listen_to_addresses())
-    asyncio.ensure_future(print_loop(wallet))
+    tasks = asyncio.gather(
+        asyncio.ensure_future(wallet.listen_to_addresses()),
+        asyncio.ensure_future(print_loop(wallet)))
 
-    loop.run_forever()
-    loop.close()
+    # Graceful shutdown code borrowed from:
+    # https://stackoverflow.com/questions/30765606/
+    # whats-the-correct-way-to-clean-up-after-an-interrupted-event-loop
+    try:
+        # Here `amain(loop)` is the core coroutine that may spawn any
+        # number of tasks
+        sys.exit(loop.run_until_complete(tasks))
+    except KeyboardInterrupt:
+        # Optionally show a message if the shutdown may take a while
+        print("\nAttempting graceful shutdown, press Ctrl+C again to exit…",
+              flush=True)
+
+        # Do not show `asyncio.CancelledError` exceptions during shutdown
+        # (a lot of these may be generated, skip this if you prefer to see them)
+        def shutdown_exception_handler(loop, context):
+            if "exception" not in context \
+            or not isinstance(context["exception"], asyncio.CancelledError):
+                loop.default_exception_handler(context)
+        loop.set_exception_handler(shutdown_exception_handler)
+
+        # Handle shutdown gracefully by waiting for all tasks to be cancelled
+        tasks = asyncio.gather(*asyncio.Task.all_tasks(loop=loop),
+                               loop=loop,
+                               return_exceptions=True)
+        tasks.add_done_callback(lambda t: loop.stop())
+        tasks.cancel()
+
+        # Keep the event loop running until it is either destroyed or all
+        # tasks have really terminated
+        while not tasks.done() and not loop.is_closed():
+            loop.run_forever()
+    finally:
+        loop.close()
 
 if __name__ == "__main__":
     main()
